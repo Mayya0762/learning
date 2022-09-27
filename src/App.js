96693be755b73cfc5251730951a802185bac1f2a
@Mayya0762 <easymai0762@mail.ru>
2022-09-27 17:11:53 +0300

    rirst commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
+import Text from './Text';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [painting, setPainting] = useState(0);
+  const {id, description, price, image, rating} = data[painting];
+  
+  const nextPainting = () => {
+    setPainting((painting => {
+      painting++;
+      if(painting > data.length - 1) {
+        painting = 0
+      }
+      return painting;
+    }))
+  }
+
+  const previousPainting = () => {
+    setPainting((painting => {
+      painting--;
+      if(painting < 0) {
+        return data.length - 1
+      }
+      return painting;
+    }))
+  }
+
+  return (<div className='main'>
+    
+    <div className='heading'>
+    <h1 className="caption">Создание картинок с помощью искусственного интеллекта</h1>
     </div>
+    
+    <div className='box'>
+    <div className="header">
+    <Text />
+    </div>
+
+    <div className='section'>
+    <div key={id}>
+    <div className="container">
+      <h2 className="caption">Посмотреть</h2>
+    </div>
+      <div className="container">
+        <img src={image} width="400px" alt="foto" />
+      </div>
+      <div className="container">
+        <button className='btn caption' onClick={previousPainting}>Назад</button>
+        <button className='btn caption' onClick={nextPainting}>Вперед</button>
+      </div>
+      <div className="container">
+        <h3>{id}. {description}</h3>
+      </div>
+      <div className="container">
+        <h3>Рейтинг: {rating}</h3>
+      </div>
+      <div className="container">
+        <h3>Цена: {price}</h3>
+      </div>
+      <div className="container">
+        <button className='caption' onClick={() => setPainting([])}>Delete All</button>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+  
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,118 @@
+import image1 from './pictures/1.jpg';
+import image2 from './pictures/2.jpg';
+import image3 from './pictures/3.jpg';
+import image4 from './pictures/4.jpg';
+import image5 from './pictures/5.jpg';
+import image6 from './pictures/6.jpg';
+import image7 from './pictures/7.jpg';
+import image8 from './pictures/8.jpg';
+import image9 from './pictures/9.jpg';
+import image10 from './pictures/10.jpg';
+import image11 from './pictures/11.jpg';
+import image12 from './pictures/12.jpg';
+import image13 from './pictures/13.jpg';
+
+export const data = [
+{
+    id: 1,
+    description: "Бассейн с видом на горы",
+    price: "Пока бесплатно!",
+    image: image1,
+    rating: 5,
+},
+
+{
+    id: 2,
+    description: "Белая луна, фэнтази",
+    price: "Пока бесплатно!",
+    image: image2,
+    rating: 5,
+},
+
+{
+    id: 3,
+    description: "Вселенная в кружке кофе",
+    price: "Пока бесплатно!",
+    image: image3,
+    rating: 4,
+},
+
+{
+    id: 4,
+    description: "Город в пустыне",
+    price: "Пока бесплатно!",
+    image: image4,
+    rating: 3,
+},
+
+{
+    id: 5,
+    description: "Город на другой планете",
+    price: "Пока бесплатно!",
+    image: image5,
+    rating: 4,
+},
+
+{
+    id: 6,
+    description: "Город на неизвестной планете",
+    price: "Пока бесплатно!",
+    image: image6,
+    rating: 5,
+},
+
+{
+    id: 7,
+    description: "Звездная ночь",
+    price: "Пока бесплатно!",
+    image: image7,
+    rating: 5,
+},
+
+{
+    id: 8,
+    description: "Подземный мир",
+    price: "Пока бесплатно!",
+    image: image8,
+    rating: 2,
+},
+
+{
+    id: 9,
+    description: "Полевые цветы",
+    price: "Пока бесплатно!",
+    image: image9,
+    rating: 4,
+},
+
+{
+    id: 10,
+    description: "Ресторан на краю вселенной",
+    price: "Пока бесплатно!",
+    image: image10,
+    rating: 5,
+},
+
+{
+    id: 11,
+    description: "Руины в одной из галактик",
+    price: "Пока бесплатно!",
+    image: image11,
+    rating: 4,
+},
+
+{
+    id: 12,
+    description: "Лес под голубым небом",
+    price: "Пока бесплатно!",
+    image: image12,
+    rating: 5,
+},
+{
+    id: 13,
+    description: "Test",
+    price: "Пока бесплатно!",
+    image: image13,
+    rating: 0,
+},
+]
